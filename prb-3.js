3ed7853e84e4a5b38008b8c64634f8f348f5ab59
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-08-11 15:15:27 +0600

    done prblm

--- prb-3.js
+++ prb-3.js
@@ -1,12 +1,24 @@
 function oilPrice(disel,petrol,octane){
 
-    if(disel==="number" && petrol==="number" && octane==="number"){
-        return "execute"
+    if(typeof(disel)==="number" && typeof(disel)==="number" && typeof(octane)==="number" ){
+       let diselPrice=114;
+       let petrolPrice=130;
+       let octanePrice=135;
+
+       let totallDiselPrice=diselPrice*disel;
+       let totallPetrolPrice= petrolPrice*petrol;
+       let totallOctanePrice=octanePrice*octane;
+
+       let OveallTotalPrice =totallDiselPrice+totallPetrolPrice+totallOctanePrice;
+
+       return OveallTotalPrice;
+
+
     }
     else{
         return "Please! Provide a valid input that will be a NUmber type.";
     }
 }
 
-let output=oilPrice(1,1,1);
+let output=oilPrice(0,2,3);
 console.log(output);
